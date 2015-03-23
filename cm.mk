c1ef8fe4081ae5d46fc8502d35511b319076bee3
Romain Caire <romain.caire@doro.com>
2015-03-23 15:21:40 +0100

    ss

--- cm.mk
+++ cm.mk
@@ -1,5 +1,11 @@
+## Specify phone tech before including full_phone
+# $(call inherit-product, vendor/cm/config/gsm.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := liberto_hero
+
+# Inherit some common CM stuff.
+# $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/doro/liberto_hero/device_liberto_hero.mk)
