6b3eb09df8bc5cf3dbc0cd89626c7123d598fa8c
Romain Caire <romain.caire@doro.com>
2015-05-12 17:39:12 +0200

    Add ION libs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Kernel includes
-# TARGET_CUSTOM_KERNEL_HEADERS 		:= device/doro/liberto_hero/include
+TARGET_CUSTOM_KERNEL_HEADERS 		:= device/doro/liberto_hero/include
 BOARD_KERNEL_CMDLINE 			:= console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
 BOARD_KERNEL_BASE        		:= 0x80000000
 BOARD_KERNEL_PAGESIZE    		:= 2048
