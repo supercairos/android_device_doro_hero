3522e91e27ba2ef3cb824c53ebf60761476dac4b
Romain Caire <romain.caire@doro.com>
2015-10-07 01:35:38 +0200

    Add GPS stuff

--- device.mk
+++ device.mk
@@ -135,6 +135,11 @@ PRODUCT_PACKAGES += \
 	
 # GPS
 PRODUCT_PACKAGES += \
+	libloc_eng \
+	libloc_core \
+	libloc_ds_api \
+	libgps.utils \
+	libloc_api_v02 \
     gps.msm8916
 	
 PRODUCT_COPY_FILES += \
