c0c53d0f090eb5488054679ff2a9899574600a79
Romain Caire <romain.caire@doro.com>
2015-11-04 13:02:04 +0100

    Fix compilation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,7 @@ TARGET_HW_DISK_ENCRYPTION := true
 
 # Dex
 ifeq ($(HOST_OS),linux)
-  ifeq ($(filter $(TARGET_BUILD_VARIANT),user userdebug))
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
