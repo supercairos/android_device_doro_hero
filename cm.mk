cdfe3ac738be2762ada99ef1eb03aa843204b4d2
Romain Caire <romain.caire@doro.com>
2015-11-06 17:14:56 +0100

    WTF v2

--- cm.mk
+++ cm.mk
@@ -9,17 +9,17 @@ $(call inherit-product, device/doro/liberto_hero/full_hero.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := HERO
+PRODUCT_RELEASE_NAME := Doro Liberto 825
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := liberto_hero
 PRODUCT_NAME := cm_liberto_hero
 PRODUCT_BRAND := Doro
-PRODUCT_MODEL := HERO
+PRODUCT_MODEL := Liberto 825
 PRODUCT_MANUFACTURER := Doro
 
 # Set product name
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=Doro Liberto 825
+    PRODUCT_NAME=Doro_Liberto_825
 	
 PRODUCT_GMS_CLIENTID_BASE := android-doro
