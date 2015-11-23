6a078e5885eaf3e25dc1067f108987a6d583b664
Romain Caire <romain.caire@doro.com>
2015-11-23 13:15:41 +0100

    Remove MKImage Tool. (reverted from commit 8e047d0fb2fc372d4389a039a3e481585b722645)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,8 @@ TARGET_KERNEL_ADDITIONAL_CONFIG := cyanogenmod_debug_config
 endif
 
 TARGET_KERNEL_SOURCE 			:= kernel/doro/msm8916
+TARGET_KERNEL_ARCH 				:= arm
+BOARD_CUSTOM_BOOTIMG_MK 		:= $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE 				:= 0x80000000
 BOARD_KERNEL_CMDLINE 			:= console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
 # BOARD_KERNEL_CMDLINE			+= androidboot.selinux=permissive
@@ -40,7 +42,6 @@ BOARD_KERNEL_PAGESIZE 			:= 2048
 BOARD_KERNEL_SEPARATED_DT 		:= true
 BOARD_KERNEL_TAGS_OFFSET 		:= 0x01E00000
 BOARD_RAMDISK_OFFSET     		:= 0x02000000
-BOARD_DTBTOOL_ARGS 				:= -2
 
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
