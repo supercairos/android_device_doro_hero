0f40a4fa3b5ca96f34693bdc7d86129822fc17cc
Romain Caire <romain.caire@doro.com>
2015-11-24 12:49:39 +0100

    Add codec infos.

--- device.mk
+++ device.mk
@@ -117,6 +117,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
     $(LOCAL_PATH)/audio/media_codecs.xml:system/etc/media_codecs.xml \
+	$(LOCAL_PATH)/audio/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/audio/media_profiles.xml:system/etc/media_profiles.xml 
 
 PRODUCT_PACKAGES += \
